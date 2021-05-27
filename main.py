fb6024acb03a24ecc32245f9a373df8f9746c1c7
Daniel Sasse <dsasse07@gmail.com>
2021-05-27 09:50:42 -0400

    Added Recommendation limits to settings Constants

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from lib.game import Game
 from lib.game_board import GameBoard
 
-SIZE = (6,6)
-PROBABILITY = 0.7
+
+SIZE = (6,6)        # Recommended  5 <= SIZE <= 15
+PROBABILITY = 0.7   # Recommended 0.3 <= PROBABILITY <= 0.8
 
 board = GameBoard(SIZE, PROBABILITY)
 screen_size = (SIZE[0] * 80, SIZE[1] * 80)
